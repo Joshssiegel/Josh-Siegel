7fb4af8737dd874f249720283f41b9447b376a85
Joshssiegel <Joshuascottsoftware@gmail.com>
2020-08-03 14:27:28 -0400

    Updates

---
+++ precache-manifest.3221cc6ba6ba440d5cff693f9b2b4547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57fdada5f9557b13fa017c726331193",
+    "url": "/Josh-Siegel/index.html"
+  },
+  {
+    "revision": "b872c8f6a8a2691721ee",
+    "url": "/Josh-Siegel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25ad5e59884444b9af40",
+    "url": "/Josh-Siegel/static/js/2.9dc07e80.chunk.js"
+  },
+  {
+    "revision": "14d3db2f0aae1bbe6b1122194ce299b3",
+    "url": "/Josh-Siegel/static/js/2.9dc07e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b872c8f6a8a2691721ee",
+    "url": "/Josh-Siegel/static/js/main.8a9692f9.chunk.js"
+  },
+  {
+    "revision": "dca3785d46ec6d39da05",
+    "url": "/Josh-Siegel/static/js/runtime-main.9fdbc87b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Josh-Siegel/static/media/logo.5d5d9eef.svg"
+  }
+]);
